986072c5c918684e6b635474def8314040c58e22
joshhwang1125 <joshhwang1125@gmail.com>
2016-02-02 17:53:58 -0800

    create controller_base, router, and session

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -1,40 +1,75 @@
 require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
+# require 'byebug'
+
 require_relative './session'
 
 class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, params = {})
+    @params = params
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
-  # Set the response status code and header
+  # Set the res status code and header
   def redirect_to(url)
+    @res['location'] = url
+    @res.status = 302
+
+    if already_built_response?
+      raise "Already rendered :("
+    else
+      @already_built_response = true
+    end
+
+    session.store_session(@res)
   end
 
-  # Populate the response with content.
-  # Set the response's content type to the given type.
+  # Populate the res with content.
+  # Set the res's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    @res['Content-Type'] = content_type.to_s
+    @res.write(content)
+
+    if already_built_response?
+      raise "Already rendered :("
+    else
+      @already_built_response = true
+    end
+
+    session.store_session(@res)
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    path = "views/#{self.class.to_s.underscore}/#{template_name}.html.erb"
+    contents = File.read(path)
+    template = ERB.new(contents).result(binding)
+    render_content(template, 'text/html')
   end
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    self.send(name)
+    render(name) unless already_built_response?
+
+    nil
   end
 end
 
--- lib/session.rb
+++ lib/session.rb
@@ -1,19 +1,27 @@
 require 'json'
+# require 'byebug'
 
 class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+
+    cookie = req.cookies['_rails_lite_app']
+    @contents = ( cookie ? JSON.parse(cookie) : {})
+    # debugger
   end
 
   def [](key)
+    @contents[key]
   end
 
   def []=(key, val)
+    @contents[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    res.set_cookie('_rails_lite_app', {path: '/', value: @contents.to_json})
   end
 end
